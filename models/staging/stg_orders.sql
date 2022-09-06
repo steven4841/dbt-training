297c94174f22671ee61cc503c4bbf67e79939721
steven4841@gmail.com <steven4841@gmail.com>
2022-09-06 15:02:28 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,19 @@
+select 
+--From Raw Orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice-o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw customer
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p on o.productid = p.productid
